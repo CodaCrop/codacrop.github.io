7eac3e82f64af71da2000f88c1d279191e343ac6
CodaCrop <wim.d.boer0172@gmail.com>
2016-11-25 16:37:21 +0100

    Added default smoothState and added some keyframe animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,28 +1,32 @@
-$(function(){
-  'use strict';
-  var options = {
-    prefetch: true,
-    cacheLength: 2,
-    onStart: {
-      duration: 250, // Duration of animation
-      render: function($container) {
-        // Add CSS animation reversing class
-        $container.addClass('is-exiting');
+$(document).ready(function() {
 
-        // restart the animation
-        smoothState.restartCSSAnimations();
-      }
-    },
-    onReady: {
-      duration: 0,
-      render: function($container, $newContent) {
-        // Remove CSS animation reversing class
-        $container.removeClass('is-exiting');
+  $(function(){
+    'use strict';
+    var $page = $('#main'),
+        options = {
+          debug: true,
+          prefetch: true,
+          cacheLength: 2,
+          onStart: {
+            duration: 2000, // Duration of our animation
+            render: function ($container) {
+              // Add your CSS animation reversing class
+              $container.addClass('is-exiting');
+              // Restart your animation
+              smoothState.restartCSSAnimations();
+            }
+          },
+          onReady: {
+            duration: 0,
+            render: function ($container, $newContent) {
+              // Remove your CSS animation reversing class
+              $container.removeClass('is-exiting');
+              // Inject the new content
+              $container.html($newContent);
+            }
+          }
+        },
+        smoothState = $page.smoothState(options).data('smoothState');
+  });
 
-        // Inject new content
-        $container.html($newContent);
-      }
-    }
-  },
-  smoothState = $('#main').smoothState(options).data('smoothState');
 });
