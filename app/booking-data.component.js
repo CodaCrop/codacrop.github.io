51cca1429856714c29fa3f4dff8f6dedca3376fc
CodaCrop <wim.d.boer0172@gmail.com>
2016-12-15 16:55:48 +0100

    Add files via upload

---
+++ app/booking-data.component.js
@@ -0,0 +1,32 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/mergeMap');
+require('rxjs/add/operator/map');
+var BookingDataComponent = (function () {
+    function BookingDataComponent(http) {
+        this.http = http;
+    }
+    BookingDataComponent.prototype.ngOnInit = function () {
+    };
+    BookingDataComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'app-root',
+            templateUrl: 'booking-data.component.html'
+        }), 
+        __metadata('design:paramtypes', [http_1.Http])
+    ], BookingDataComponent);
+    return BookingDataComponent;
+}());
+exports.BookingDataComponent = BookingDataComponent;
+//# sourceMappingURL=booking-data.component.js.map
