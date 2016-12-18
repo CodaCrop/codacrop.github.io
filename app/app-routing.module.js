af38a73f6a0fb6bbb606f9160475c9d2c139e5d6
CodaCrop <wim.d.boer0172@gmail.com>
2016-12-18 21:11:50 +0100

    Responsivess fixes and routing adjustments

--- app/app-routing.module.js
+++ app/app-routing.module.js
@@ -1,6 +1,6 @@
 "use strict";
-var booking_component_1 = require('./booking.component');
-var booking_data_component_1 = require('./booking-data.component');
+var booking_component_1 = require("./booking.component");
+var booking_data_component_1 = require("./booking-data.component");
 // Routing. Default page will be ran on booking.component
 exports.AppRoutes = [
     { path: '', redirectTo: 'booking', pathMatch: 'full' },
